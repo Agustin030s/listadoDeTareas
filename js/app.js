dcfd9184492e104d3366239f9ad47f38bce2a398
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-11-22 18:23:38 -0300

    se realizo con exito el ejercicio 2

---
+++ js/app.js
@@ -0,0 +1,47 @@
+const formTarea = document.querySelector('#formTarea');
+const contenedorCards = document.getElementById('contenedorCards');
+const btnClose = document.querySelector('#btnClose');
+const listaDeTareas = [];
+
+const crearLista = (textoTarea, indice) => {
+    let contenedor = document.createElement('div');
+    contenedor.className = 'col-md-4 col-lg-3';
+    contenedor.id = indice;
+
+    let cardTarea = `<div class="card mt-2">
+    <div class="card-header d-flex justify-content-between align-items-center">
+        <p class="m-0">📌</p>
+        <button class="btn btn-close" id="btnEliminar" onclick="eliminarTarea(${indice})"></button>
+    </div>
+    <div class="card-body">
+        <p>${textoTarea}</p>
+    </div>
+</div>`;
+
+    contenedor.innerHTML = cardTarea;
+
+    contenedorCards.appendChild(contenedor);
+    document.querySelector('#tarea').value = "";
+}
+
+const addTarea = (e) =>{
+    e.preventDefault();
+    const textoTarea = document.getElementById('tarea').value;
+    listaDeTareas.push(textoTarea);
+
+    console.log(textoTarea);
+
+    crearLista(textoTarea, listaDeTareas.length -1);
+}
+
+const eliminarTarea = (indice) => {
+    // Eliminar la tarea del array
+    listaDeTareas.splice(indice, 1);
+
+    // Eliminar la tarea del DOM
+    const tareaEliminada = document.getElementById(indice);
+    console.log(tareaEliminada);
+    tareaEliminada.remove();
+}
+
+formTarea.addEventListener('submit', addTarea);
